a36671e8011f5dfc2add0039e32807971a2fc1df
K41111NUX <79648002+Senpai-sama-afk@users.noreply.github.com>
2021-09-19 19:11:30 +0400

    Update call_back_button_handler.py

--- bot/plugins/call_back_button_handler.py
+++ bot/plugins/call_back_button_handler.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# (c) Shrimadhav U K | gautamajay52 | @AbirHasan2005
-
-# the logging things
-
-from bot.helper_funcs.utils import(
-    delete_downloads
+from bot.helper_funcs.utils import on_task_complete, add_task
+from bot import (
+    AUTH_USERS,
+    DOWNLOAD_LOCATION,
+    LOG_CHANNEL,
+    data,
+    pid_list
 )
+from pyrogram.types import CallbackQuery
+import datetime
 import logging
-import os
+import os, signal
 import json
 import shutil
 logging.basicConfig(
@@ -17,14 +18,8 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-import datetime
-from pyrogram.types import CallbackQuery
 #from bot.helper_funcs.admin_check import AdminCheck
-from bot import (
-    AUTH_USERS,
-    DOWNLOAD_LOCATION,
-    LOG_CHANNEL
-)
+
 
 async def button(bot, update: CallbackQuery):
     cb_data = update.data
@@ -46,34 +41,34 @@ async def button(bot, update: CallbackQuery):
                     json.dump(statusMsg, f, indent=2)
                     if 'pid' in statusMsg.keys():
                         try:
-                            os.kill(statusMsg["pid"], 9)
-                        except:
+                            os.kill(statusMsg["pid"], signal.SIGSTOP)
+                            os.kill(pid_list[0], signal.SIGSTOP) 
+                            del pid_list[0]
+                            os.system("rm -rf downloads/*")
+                            await bot.delete_messages(update.message.chat.id, statusMsg["message"])
+                            #await on_task_complete()
+                        except Exception as e:
+                            print(e)
                             pass
-                        delete_downloads()
-                    try:
-                        await bot.delete_messages(update.message.chat.id, statusMsg["message"])
-                    except:
-                        pass
-                    try:
-                        await update.message.edit_text("🚦🚦 Last Process Stopped 🚦🚦")
+                        
                         chat_id = LOG_CHANNEL
                         utc_now = datetime.datetime.utcnow()
-                        ist_now = utc_now + datetime.timedelta(minutes=30, hours=5)
+                        ist_now = utc_now + \
+                            datetime.timedelta(minutes=30, hours=5)
                         ist = ist_now.strftime("%d/%m/%Y, %H:%M:%S")
-                        bst_now = utc_now + datetime.timedelta(minutes=00, hours=6)
+                        bst_now = utc_now + \
+                            datetime.timedelta(minutes=00, hours=6)
                         bst = bst_now.strftime("%d/%m/%Y, %H:%M:%S")
                         now = f"\n{ist} (GMT+05:30)`\n`{bst} (GMT+06:00)"
                         await bot.send_message(chat_id, f"**Last Process Cancelled, Bot is Free Now !!** \n\nProcess Done at `{now}`", parse_mode="markdown")
-                    except:
-                        pass
             else:
                 try:
                     await update.message.edit_text("You are not allowed to do that 🤭")
                 except:
                     pass
+
         elif cb_data == "fuckoff":
             try:
                 await update.message.edit_text("Okay! Fine 🤬")
             except:
                 pass
-				
